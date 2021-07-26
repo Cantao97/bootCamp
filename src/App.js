929b4028eda508458bc1d78e471ed41051369ba0
unknown <gcantao97@gmail.com>
2021-07-26 10:57:11 -0300

    primeiro commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+//import './App.css';
+
+function App() {
+  return (
+    <div 
+      style={{
+        width: '400px',
+        height: "200px",
+
+      }}
+    >
+      <p> Projeto 1 </p>
+    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
